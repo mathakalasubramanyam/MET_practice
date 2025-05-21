72f893a99da79c8274a3c682f94847a490d2a67d
mathakalasubramanyam <mathakalasubbu856@gmail.com>
2025-05-21 19:06:22 +0530

    first

---
+++ MIT FOLDER/ASSIGNMENTS/total bill for item.py/billamount.py
@@ -0,0 +1,7 @@
+items=["idly","vada","puri"]
+prices=(10,10,10)
+customer=input("enter the item name:")
+count=input("enter the item count:")
+if customer==items[0]:
+    a=f"{count}*prices(0)"
+    print(a)
